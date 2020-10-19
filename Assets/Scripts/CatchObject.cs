d920d7732d165e1441d1c261d7fddbce2f24e37e
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2020-10-19 08:23:26 +0700

    first blood

---
+++ Assets/Scripts/CatchObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchObject : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject hand;
+    bool catched = false;
+    Vector3 ballPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballPosition = ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!catched)
+            {
+                ball.transform.SetParent(hand.transform);
+                ball.transform.localPosition = hand.transform.localPosition;
+                catched = true;
+                ball.GetComponent<Renderer>().material.color = Color.yellow;
+            } else
+            {
+                ball.transform.SetParent(null);
+                ball.transform.localPosition = ballPosition;
+                catched = false;
+                ball.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MoveScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour
+{
+    public void GoTo(string page) {
+        SceneManager.LoadScene(page);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
